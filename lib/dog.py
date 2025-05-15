20c640ddb7a3fc41498bebd1091ffa215d1639f0
ken <kentuei05@gmail.com>
2025-05-15 12:49:11 +0300

    property funtion

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
 
 APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+    "Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle",
+    "French Bulldog", "Pug", "Pointer"
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Mutt"):
+        self.name = name
+        # Only set breed if name was valid and self._name exists
+        if hasattr(self, '_name'):
+            self.breed = breed
+
+    # name property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # breed property
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Person", job="Admin"):
+        self.name = name  # uses setter
+        self.job = job    # uses setter
+
+    # name property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # job property
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
